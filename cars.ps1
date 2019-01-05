3b91f737b3dc5b59544d13f5712080b5a7f8687a
strudel28 <mashicus.ne@gmail.com>
2019-01-05 18:20:25 +0300

    add old files

---
+++ cars.ps1
@@ -0,0 +1,9 @@
+﻿class Car{
+    [int] $Wheels = 0
+    [string] $Color = ""
+}
+$MyCar = New-Object Car
+$MyCar.Wheels=4
+$MyCar.Color = "Red"
+
+$MyCar | ConvertTo-Json | out-file C:\Users\Maryia\ps-learn\MyRed.json
