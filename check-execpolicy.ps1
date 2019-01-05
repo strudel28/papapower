5c3d52d6d5f13b5d4f8b896d81e9110019fa7e0d
strudel28 <mashicus.ne@gmail.com>
2019-01-05 19:14:36 +0300

    code to check exec policy

---
+++ check-execpolicy.ps1
@@ -0,0 +1,8 @@
+﻿# This code allows to check execution policy
+$policy = Get-ExecutionPolicy
+if ( $policy = "Restricted" )
+{ Write-Output "no to run script"
+  Set-ExecutionPolicy unrestricted
+}
+else
+{ Write-Output "ok to run script"}
